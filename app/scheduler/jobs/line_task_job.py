b702a35c0ff51a231ac82bcfaff2486ee40ca535
yu-dai916 <respectyou0916@icloud.com>
2026-06-29 11:35:48 +0900

    Update line_task_job.py

--- app/scheduler/jobs/line_task_job.py
+++ app/scheduler/jobs/line_task_job.py
@@ -1,7 +1,7 @@
 def run():
     import os
 
-    print("🔥🔥🔥 DATABASE_URL 👉", os.getenv("DATABASE_URL"))
+    print("DATABASE_URL:", os.getenv("DATABASE_URL"))  # ←追加
 
     from app.db import SessionLocal
     from app.models.review import Review
@@ -18,7 +18,3 @@ def run():
     send_line(msg)
 
     db.close()
-
-
-if __name__ == "__main__":
-    run()
