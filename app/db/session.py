e94fd4b3689377ff5248fdd735381956c778f7a3
yu-dai916 <respectyou0916@icloud.com>
2026-06-29 14:09:16 +0900

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -2,15 +2,21 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+print("🔥🔥🔥 session.py 読まれてる 🔥🔥🔥")
+
+# 環境変数取得
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("🔥 env DATABASE_URL =", DATABASE_URL)
 
 # 🔥 強制 fallback（最終手段）
 if not DATABASE_URL:
     print("⚠️ env取れてないから直書き使う")
     DATABASE_URL = "postgresql://postgres:Yudaifuji916%21@db.dmjzoetznpfcicwvujbt.supabase.co:5432/postgres"
 
-print("🔥 DATABASE_URL =", DATABASE_URL)
+print("🔥 最終 DATABASE_URL =", DATABASE_URL)
 
+# DB接続
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
