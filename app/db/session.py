aca406ba5fbefb62d19ff40445c80d6c740bf79b
yu-dai916 <respectyou0916@icloud.com>
2026-06-29 12:28:53 +0900

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -4,8 +4,10 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+print("🔥 DATABASE_URL =", DATABASE_URL)  # ←追加
+
 if not DATABASE_URL:
-    raise Exception("DATABASE_URLが設定されてない")
+    raise Exception("❌ DATABASE_URLが空")
 
 engine = create_engine(DATABASE_URL)
 
