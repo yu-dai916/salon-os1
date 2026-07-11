8b7841f439e0fc018b0d5d6bdc93baee1696d257
yu-dai916 <respectyou0916@icloud.com>
2026-07-11 21:07:25 +0900

    Update line_notify.py

--- app/services/line_notify.py
+++ app/services/line_notify.py
@@ -1,9 +1,10 @@
 import requests
+import os
 
-LINE_TOKEN = "3ff6GH2IkKi7aYuvCrmCQJEA2f3AfT7PODAs3hl/h989dDhhsHGJMdPRU9HuYxCGEVQgP45ktU05f1GYh1nUACzAeP6jvJFOuKdZ4EAkbrU6tWklDdb6YsyZc6r0aFPqy6QtOmfljbCeG0Y+BpMVlgdB04t89/1O/w1cDnyilFU="
-USER_ID = "Uea9d5bf991230a9ff066272797da6cae"
+LINE_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 
-def send_line(message):
+
+def send_line(message, user_id):
     url = "https://api.line.me/v2/bot/message/push"
 
     headers = {
@@ -12,7 +13,7 @@ def send_line(message):
     }
 
     data = {
-        "to": USER_ID,
+        "to": user_id,  # 🔥 ここが核心
         "messages": [
             {"type": "text", "text": message}
         ]
