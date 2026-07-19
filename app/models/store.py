0631e1ff906cfb8e89e2c28c1de7798ccbed0195
yu-dai916 <respectyou0916@icloud.com>
2026-07-19 12:32:10 +0900

    Update store.py

--- app/models/store.py
+++ app/models/store.py
@@ -6,6 +6,7 @@ class Store(Base):
     __tablename__ = "stores"
 
     id = Column(Integer, primary_key=True, index=True)
+    store_code = Column(String, unique=True, index=True, nullable=True)  # ←追加
     name = Column(String)
     place_id = Column(String)
     created_at = Column(TIMESTAMP, server_default=text("now()"))
