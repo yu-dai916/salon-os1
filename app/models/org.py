000864255a2c7959d97e5ce468908d588c06a5c5
yu-dai916 <respectyou0916@icloud.com>
2026-07-24 11:14:13 +0900

    Update org.py

--- app/models/org.py
+++ app/models/org.py
@@ -1,11 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from app.db.base import Base
 
 
 class Org(Base):
-
     __tablename__ = "orgs"
 
     id = Column(Integer, primary_key=True)
+
+    agency_id = Column(
+        Integer,
+        ForeignKey("agencies.id"),
+        nullable=True,
+    )
 
     name = Column(String(255))
